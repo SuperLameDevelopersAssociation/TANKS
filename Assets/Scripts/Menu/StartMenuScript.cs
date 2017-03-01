8d20cc95079634ca585aa3a27dfa0e9dace85d3b
Koesterc <Koesterc@gmatc.matc.edu>
2017-02-28 22:59:24 -0600

    Menu
    create a canvas
    inside canvas create 3 children
    1 is a frame and the other tw are butttons
    Create a Play button and a Quit button
    Drag the "StartMenuScript" into canvas
    start button will, in the event system, call the starmenuscript Lobby()
    where as quit will call the StartMenuScript Quit()
    
    Any questions let me know - Chris

---
+++ Assets/Scripts/Menu/StartMenuScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class StartMenuScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void Lobby()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
+
+    // Use this for initialization
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
