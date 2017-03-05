14e12a497a35dd9404e455df70839093660004ff
PeterHoppers <hoppepj@gmatc.matc.edu>
2017-03-05 15:57:57 -0600

    Merged the Branch "Menu" into Development

---
+++ Assets/Scripts/Menu/StartMenuScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class StartMenuScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject menuPanel;
+
+    // Use this for initialization
+    public void Lobby()
+    {//this takes you to nickname
+        menuPanel.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void Return()
+    {//returns to main menu
+        gameObject.SetActive(true);
+        menuPanel.SetActive(false);
+    }
+
+    public void ExitGame()
+    {//quits the damn game
+        Application.Quit();
+    }
+
+
+}
