da2a564102a6e683fff4af600a9b17b7d2d17505
PeterHoppers <hoppepj@gmatc.matc.edu>
2017-03-05 19:32:49 -0600

    Brought over all of Jeff's Art Stuff, not merged yet

---
+++ Assets/ArcReactor/Scripts/Utils/ArcReactor_EmitterDestructor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArcReactor_EmitterDestructor : MonoBehaviour {
+
+	public ParticleSystem partSystem;
+	public bool onlyDisable;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!partSystem.IsAlive())
+		{
+			if (onlyDisable)
+			{
+				gameObject.SetActive(false);
+				enabled = false;
+			}
+			else
+				Destroy(gameObject);
+		}
+	}
+}
