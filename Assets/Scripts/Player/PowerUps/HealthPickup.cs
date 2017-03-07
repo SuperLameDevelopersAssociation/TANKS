fb7ffc64fd8efb0fcb93eac2c13f8cd96f78ffea
jbeckman82 <beckmajm@gmatc.matc.edu>
2017-03-07 10:03:23 -0600

    Script Change
    The script now does not allow you to pick up the health power up unless
    you have a health count less than the max health

--- Assets/Scripts/Player/PowerUps/HealthPickup.cs
+++ Assets/Scripts/Player/PowerUps/HealthPickup.cs
@@ -4,19 +4,19 @@ using TrueSync;
 
 public class HealthPickup : MonoBehaviour 
 {
-	Health playerCurrHealth;
+	Health playerHealth;
 
 	public void OnSyncedTriggerEnter(TSCollision other)
 	{
 		if (other.gameObject.tag == "Player")   //Checks if collided with player
 		{
-			playerCurrHealth = other.gameObject.GetComponent<Health>();
+			playerHealth = other.gameObject.GetComponent<Health>();
 
-			if (playerCurrHealth == null) //checks to see if the health script is attached
+			if (playerHealth == null) //checks to see if the health script is attached
 				Debug.LogError("There is no health script on " + other.gameObject.name);
-			else
+			else if (playerHealth.currHealth != playerHealth.maxHealth)
 			{
-				playerCurrHealth.currHealth += 15;
+				playerHealth.currHealth += 15;
 				TrueSyncManager.SyncedDestroy(this.gameObject);
 			}
 		}
