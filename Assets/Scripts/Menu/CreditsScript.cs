bfda4833c54609bfc996f87ca5b7d1100c030100
Koesterc <Koesterc@gmatc.matc.edu>
2017-03-11 22:20:46 -0600

    credits
    requirements:
    Update startmenu script
    CreditScreen prefab
    CreditsSript
    CreditsSceenController
    Credits.anim
    
    Instructions:
    Open on the Menu sceen
    Bring in the new CreditScreen prefab
    and Insert it as a child of GUI
    assign it the CreditScript
    Drag Main Menu into the designate slot of that script
    Now take the creditscript game object (that you just made) and insert it
    into the designated slot of Main Menu game object.
    Afterwards, make sure the CreditScreen gameobject has the controller,
    and the Credits.anim inserted intot hat controller.
    
    Finally, create a new button and call it "credits"
    In the event system for this button call the StartMenscript.Credits()
    
    That should be all. Any questions let me know
    
    -Chris

---
+++ Assets/Scripts/Menu/CreditsScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsScript : MonoBehaviour {
+    [SerializeField]
+    GameObject mainMenu;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	private void Update()
+    {
+        if (Input.anyKey && gameObject.activeSelf)//this allows us to skip the menu
+            gameObject.GetComponent<Animator>().speed = 10.0f;
+    }
+    public void EndCredits()
+    {
+        mainMenu.SetActive(true);
+        gameObject.GetComponent<Animator>().speed = 1.0f;
+        gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Menu/StartMenuScript.cs
+++ Assets/Scripts/Menu/StartMenuScript.cs
@@ -4,10 +4,12 @@ public class StartMenuScript : MonoBehaviour
 {
     [SerializeField]
     GameObject menuPanel;
+    [SerializeField]
+    GameObject creditsScreen;
 
     // Use this for initialization
     public void Lobby()
-    {//this takes you to nickname
+    {//this takes you to user nickname
         menuPanel.SetActive(true);
         gameObject.SetActive(false);
     }
@@ -23,5 +25,11 @@ public class StartMenuScript : MonoBehaviour
         Application.Quit();
     }
 
-
-}
+    public void Credits()
+    {//let the credits roll, baby!
+        gameObject.SetActive(false);
+        creditsScreen.SetActive(true);
+        creditsScreen.GetComponent<Animator>().Play("Credits");
+        creditsScreen.GetComponent<Animator>().speed = 1.0f;
+    }
+}//end of class
