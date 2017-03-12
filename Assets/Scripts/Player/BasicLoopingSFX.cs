108b1e14bdfeee5b57335c46070016b4d1070ccb
PeterHoppers <hoppepj@gmatc.matc.edu>
2017-03-12 12:16:47 -0500

    Implemented SFX

---
+++ Assets/Scripts/Player/BasicLoopingSFX.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using TrueSync;
+using System.Collections;
+
+public class BasicLoopingSFX : TrueSyncBehaviour{
+
+    public AudioSource source;
+    public AudioClip sfx;
+
+
+    public override void OnSyncedStart()    // yes, I know you could set all this in Unity, but redunency is nice, no?
+    {
+        source.loop = true;
+        source.clip = sfx;
+
+        source.Play();
+    }
+}
