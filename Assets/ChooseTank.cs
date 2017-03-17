0304a881d8581164097bbb744c3eb51c42d60632
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-03-17 15:23:46 -0500

    Revert "Revert "ChooseTank""
    This reverts commit 99fa0e7412b6dad9eabf55d2a4720a44efc84d5a.

---
+++ Assets/ChooseTank.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChooseTank : MonoBehaviour 
+{
+	PlayerManager playerManager;
+
+	void Start () 
+	{
+		playerManager = GameObject.Find ("GameManager").GetComponent<PlayerManager> ();
+	}
+
+	public void ChooseMyTank(int tankIChoose)
+	{
+		playerManager.SetUpPlayers (PhotonNetwork.player, tankIChoose);
+	}
+}
