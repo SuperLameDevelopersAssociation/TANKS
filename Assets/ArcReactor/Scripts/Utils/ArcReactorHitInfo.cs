c1865ffa98ce5d038e0d8d0914d1cf75437a16ae
PeterHoppers <hoppepj@gmatc.matc.edu>
2017-03-19 15:27:50 -0500

    Tounched Up Folders. Added Hedgehogs.

--- Assets/ArcReactor/Scripts/Utils/ArcReactorHitInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class ArcReactorHitInfo
-{
-	public ArcReactor_Launcher launcher;
-	public ArcReactor_Launcher.RayInfo rayInfo;
-	public RaycastHit raycastHit;
-}
--- Assets/ArcReactor/Scripts/Utils/ArcReactorHitInfo2D.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class ArcReactorHitInfo2D
-{
-	public ArcReactor_Launcher2D launcher;
-	public ArcReactor_Launcher2D.RayInfo rayInfo;
-	public RaycastHit2D raycastHit;
-}
