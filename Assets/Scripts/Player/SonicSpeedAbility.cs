4ab2d860cd8f3455a2c588bae7b6abecfc8fae3b
jbeckman82 <beckmajm@gmatc.matc.edu>
2017-04-04 08:35:22 -0500

    Script Added, Not complete

---
+++ Assets/Scripts/Player/SonicSpeedAbility.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using TrueSync;
+
+public class SonicSpeedAbility :TrueSyncBehaviour
+{
+    public KeyCode abilityKey;
+    public float duration;
+    PlayerMovement speed;
+
+    public override void OnSyncedStart()
+    {
+        speed = gameObject.GetComponent<PlayerMovement>();
+    }
+}
