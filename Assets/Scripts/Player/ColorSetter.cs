c20105f7eb73102a493d80036e37ebd10ffcecbd
PeterHoppers <hoppepj@gmatc.matc.edu>
2017-04-12 10:37:06 -0500

    Fixed issue where you can set the alpha to 0 for the color

--- Assets/Scripts/Player/ColorSetter.cs
+++ Assets/Scripts/Player/ColorSetter.cs
@@ -5,31 +5,27 @@ using System.Collections;
 public class ColorSetter : NetworkBehaviour
 {
     public Color32 tankColor;
-    public Renderer[] originalChildrenRender;                       //Grab the current children render so they can be reset after ability is done
-    Renderer[] cloakedChildrenRender;                               //copy of the current children renders so they can be changed to cloaking ability
-    public Material[] mats;                                         //gather all materials setup for the cloaking
+    Renderer[] originalChildrenRender;                       //Grab the current children render so they can be reset after ability is done
+    Renderer[] cloakedChildrenRender;                        //copy of the current children renders so they can be changed to cloaking ability
+    Material[] mats;                                         //gather all materials setup for the cloaking
 
     void Awake()
     {
+        if (tankColor.a == 0) tankColor.a = 255;
+
         originalChildrenRender = GetComponentsInChildren<Renderer>();
         mats = new Material[originalChildrenRender.Length];         //Set length of array
-        //_cooldown = cooldown;
         cloakedChildrenRender = originalChildrenRender;             //Copy array to second array
         for (int i = 0; i < originalChildrenRender.Length; i++)
             mats[i] = originalChildrenRender[i].material;           //Set mats array to originalChildrenRender
 
         for (int i = 0; i < cloakedChildrenRender.Length; i++)
         {
-            cloakedChildrenRender[i].material.color = tankColor;                      //Change materials to cloaked material
+            cloakedChildrenRender[i].material.SetColor("_Color", tankColor);                     //Change materials to cloaked material
         }
 
         CmdFindObjects();
         CmdServerColor();
-    }
-
-    void Start()
-    {
-        
     }
 
     [Command]
