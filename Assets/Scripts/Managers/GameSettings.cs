f7df633a03a105a11b76bb521f97f3645ba22011
gebharsj <gebharsj@gmatc.matc.edu>
2017-04-18 09:06:46 -0500

    added display settings

---
+++ Assets/Scripts/Managers/GameSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSettings {
+    public bool fullscreen;
+    public int textureQuality;
+    public int antialiasing;
+    public int vSync;
+    public int resolutionIndex;
+}
