a94b941de41842d4ae850ce003b5575d0a911c8b
PeterHoppers <hoppepj@gmatc.matc.edu>
2017-05-08 13:56:46 -0500

    Merged Lobby Music Into Dev

---
+++ Assets/Scripts/Menu/LobbySoundCheck.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LobbySoundCheck : MonoBehaviour
+{
+    public GameObject menuSound;
+    public GameObject lobbySound;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        lobbySound.SetActive(!menuSound.activeSelf);
+	}
+}
