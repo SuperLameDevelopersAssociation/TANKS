230c03afa7cbc3214095b0138a66ab6d82c09140
PeterHoppers <hoppepj@gmatc.matc.edu>
2017-05-11 12:26:19 -0500

    Added Text To Explain Powerups

--- Assets/Scripts/Player/PowerUpVisual.cs
+++ Assets/Scripts/Player/PowerUpVisual.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 using System.Collections;
 
@@ -10,6 +11,14 @@ public class PowerUpVisual : NetworkBehaviour
     public GameObject shieldBubble;
     public AudioClip powerUpActivateSFX;
     public AudioClip powerUpDeactivateSFX;
+    public Text powerUpText;
+    public float timeText = 1.5f;
+
+    void Start()
+    {
+        powerUpText.gameObject.SetActive(false);
+    }
+
 
     [Command]
     public void CmdSetBubble(bool isActive, string powerupType)
@@ -17,11 +26,31 @@ public class PowerUpVisual : NetworkBehaviour
         //Debug.LogError("Turn tank bubble : " + isActive + " with the type of " + powerupType);
 
         if (powerupType.Equals("Health"))
+        {
             healthBubble.SetActive(isActive);
+
+            if (isActive)
+                StartCoroutine(DisplayText("Health Restored!"));
+        }
         else if (powerupType.Equals("Damage"))
+        {
             damageBubble.SetActive(isActive);
+
+            if (isActive)
+                StartCoroutine(DisplayText("Damage Boosted!"));
+            else
+                StartCoroutine(DisplayText("Boost Ended!"));
+        }
         else if (powerupType.Equals("Shield"))
+        {
             shieldBubble.SetActive(isActive);
+
+            if (isActive)
+                StartCoroutine(DisplayText("Shield Activated!"));
+            else
+                StartCoroutine(DisplayText("Shield Destroyed!"));
+        }
+            
 
         if (isActive)
             powerUpAudio.clip = powerUpActivateSFX;
@@ -38,11 +67,30 @@ public class PowerUpVisual : NetworkBehaviour
     void RpcSetBubble(bool isActive, string powerupType)
     {
         if (powerupType.Equals("Health"))
+        {
             healthBubble.SetActive(isActive);
+
+            if (isActive)
+                StartCoroutine(DisplayText("Health Restored!"));
+        }
         else if (powerupType.Equals("Damage"))
+        {
             damageBubble.SetActive(isActive);
+
+            if (isActive)
+                StartCoroutine(DisplayText("Damage Boosted!"));
+            else
+                StartCoroutine(DisplayText("Boost Ended!"));
+        }
         else if (powerupType.Equals("Shield"))
+        {
             shieldBubble.SetActive(isActive);
+
+            if (isActive)
+                StartCoroutine(DisplayText("Shield Activated!"));
+            else
+                StartCoroutine(DisplayText("Shield Destroyed!"));
+        }
 
         if (isActive)
             powerUpAudio.clip = powerUpActivateSFX;
@@ -52,6 +100,14 @@ public class PowerUpVisual : NetworkBehaviour
 
         powerUpAudio.Play();
 
+    }
+
+    IEnumerator DisplayText(string message)
+    {
+        powerUpText.gameObject.SetActive(true);
+        powerUpText.text = message;
+        yield return new WaitForSeconds(timeText);
+        powerUpText.gameObject.SetActive(false);
     }
 
 
