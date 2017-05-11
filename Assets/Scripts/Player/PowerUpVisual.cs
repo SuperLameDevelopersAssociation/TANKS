9f32343ed919bca12f4473c6d5c842befcce735b
PeterHoppers <hoppepj@gmatc.matc.edu>
2017-05-11 11:58:29 -0500

    Fixed Stacking Powerup Effects

---
+++ Assets/Scripts/Player/PowerUpVisual.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PowerUpVisual : NetworkBehaviour
+{
+    public AudioSource powerUpAudio;
+    public GameObject healthBubble;
+    public GameObject damageBubble;
+    public GameObject shieldBubble;
+    public AudioClip powerUpActivateSFX;
+    public AudioClip powerUpDeactivateSFX;
+
+    [Command]
+    public void CmdSetBubble(bool isActive, string powerupType)
+    {
+        //Debug.LogError("Turn tank bubble : " + isActive + " with the type of " + powerupType);
+
+        if (powerupType.Equals("Health"))
+            healthBubble.SetActive(isActive);
+        else if (powerupType.Equals("Damage"))
+            damageBubble.SetActive(isActive);
+        else if (powerupType.Equals("Shield"))
+            shieldBubble.SetActive(isActive);
+
+        if (isActive)
+            powerUpAudio.clip = powerUpActivateSFX;
+        else
+            powerUpAudio.clip = powerUpDeactivateSFX;
+
+
+        powerUpAudio.Play();
+
+        RpcSetBubble(isActive, powerupType);
+    }
+
+    [ClientRpc]
+    void RpcSetBubble(bool isActive, string powerupType)
+    {
+        if (powerupType.Equals("Health"))
+            healthBubble.SetActive(isActive);
+        else if (powerupType.Equals("Damage"))
+            damageBubble.SetActive(isActive);
+        else if (powerupType.Equals("Shield"))
+            shieldBubble.SetActive(isActive);
+
+        if (isActive)
+            powerUpAudio.clip = powerUpActivateSFX;
+        else
+            powerUpAudio.clip = powerUpDeactivateSFX;
+
+
+        powerUpAudio.Play();
+
+    }
+
+
+}
